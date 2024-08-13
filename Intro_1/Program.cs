172318eae1b5aa42ef715fa68ee0d7d4724628a3
sagarshingare900 <sagarshingare900@gmail.com>
2024-08-13 17:40:35 +0530

    Intro

---
+++ Intro_1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//My File
+
+Console.WriteLine("Welcome to Calculator App");
+Console.WriteLine("Enter The First Number");
+int num1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Second Number");
+int num2=Convert.ToInt32(Console.ReadLine());   
+
+int sum = num1 + num2;
+Console.WriteLine($"Addition of Two Number  = {sum}");
+
+Console.WriteLine("Thank You Visit Again.....");
+
+Console.ReadLine();
